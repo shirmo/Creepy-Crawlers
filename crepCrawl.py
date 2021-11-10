ae2f17c2cffb455b850a67dbc9ff6501d220edf0
Shir <shir@Shirs-MacBook-Pro.local>
2021-11-10 12:05:20 +0200

    reform skeleton

--- crepCrawl.py
+++ crepCrawl.py
@@ -2,19 +2,17 @@ class articleParams:
     def __init__(self, id, BSobj):
         self.id = id
         self.html = BSobj
+        self.articleRef = self.setArticleRef()
         self.toJson = {'id': self.id}
 
         self.creator = self.setCreator()
         self.title = self.setTitle()
+        self.text = self.setText()
         self.dollarsPledged = self.setDollarsPledged()
+        self.dollarsGoal = self.setDollarsGoal()
+        self.numBackers = self.setNumBackers()
         self.daysToGo = self.setDaysToGo()
-
-        self.articleRef = ''
-
-        self.text = ''
-        self.dollarsGoal = ''
-        self.numBackers = ''
-        self.allOrNothing = ''
+        self.allOrNothing = self.setAllOrNothing()
 
     def setCreator(self):
         pass
@@ -30,7 +28,10 @@ class articleParams:
 
     def setDollarsGoal(self):
         pass
-
+    
+    def setNumBackers(self):
+        pass
+    
     def setDaysToGo(self):
         pass
 
