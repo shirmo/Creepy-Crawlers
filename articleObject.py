a0f9e642eb249f9469f7c5726603700de74ab6e6
tyichye <tal.19595@gmail.com>
2021-11-11 10:59:29 +0200

    update fields according to html

--- articleObject.py
+++ articleObject.py
@@ -1,56 +1,99 @@
-class articleParams:
-    def __init__(self, BSobj):
-        self.json = {}
-        self.html = BSobj
-        # אפשר לא להחזיק את הפרמטרים ופשוט להכניס אותם לתוך הJSON לפי הסדר בכל פונקציה
+import json
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from lxml.html import fromstring
 
-        self.articleRef = self.setArticleRef()
 
-        self.id = self.setId()
-        self.creator = self.setCreator()
-        self.title = self.setTitle()
-        self.dollarsPledged = self.setDollarsPledged()
-        self.dollarsGoal = self.setDollarsGoal()
-        self.numBackers = self.setNumBackers()
-        self.daysToGo = self.setDaysToGo()
+class ArticleObject:
+    def __init__(self, bs_obj_main_page):
 
-        self.allOrNothing = self.setAllOrNothing()
-        self.text = self.setText()
+        self.data_project = json.loads(bs_obj_main_page.get('data-project'))
+        self.project_dict = dict()
+        self.page_soup = None
+        self.properties_dict = None
 
-    def setId(self):
-        pass
+        self.set_id()
+        self.set_url()
+        self.set_page_soup_and_properties_dict()
+        self.set_creator()
+        self.set_title()
+        self.set_dollars_pledged()
+        self.set_dollars_goal()
+        self.set_num_backers()
+        self.set_days_to_go()
+        self.set_all_or_nothing()
+        self.set_text()
 
-    def setCreator(self):
-        pass
+    def set_url(self):
+        self.project_dict['url'] = self.data_project.get('urls').get('web').get('project')
 
-    def setTitle(self):
-        pass
+    def set_id(self):
+        self.project_dict['id'] = self.data_project.get('id')
 
-    def setText(self):
-        pass
+    def set_creator(self):
+        self.project_dict['Creator'] = self.data_project.get('creator').get('name')
 
-    def setDollarsPledged(self):
-        pass
+    def set_title(self):
+        self.project_dict['Title'] = self.data_project.get('name')
 
-    def setDollarsGoal(self):
-        pass
+    def set_text(self):
+        session = requests.session()
+        html_page = session.get(self.get_url())
+        page = fromstring(html_page.content)
+
+        graph_slug = self.get_url().split("projects/")[1].split("?")[0]
+        graph_data = [{
+            "operationName": "Campaign",
+            "variables": {
+                "slug": graph_slug
+            },
+            "query": "query Campaign($slug: String!) {\n  project(slug: $slug) "
+                     "{\n    id\n    isSharingProjectBudget\n    risks\n    showRisksTab\n    "
+                     "story(assetWidth: 680)\n    currency\n    spreadsheet {\n      displayMode\n      "
+                     "public\n      url\n      data {\n        name\n        value\n        phase\n        "
+                     "rowNum\n        __typename\n      }\n      dataLastUpdatedAt\n      __typename\n    }\n    "
+                     "environmentalCommitments {\n      id\n      commitmentCategory\n      description\n      "
+                     "__typename\n    }\n    __typename\n  }\n}\n"
+        }]
+
+        csrf_token = page.xpath('//meta[@name="csrf-token"]')[0].get('content')
+        headers = dict()
+        headers['x-csrf-token'] = csrf_token
+
+        response = session.post("https://www.kickstarter.com/graph", json=graph_data, headers=headers)
+        graph_json = response.json()
+        story = graph_json[0]['data']['project']['story']
+        soup = BeautifulSoup(story, 'lxml')
+        self.project_dict['Text'] = soup.text
+
+    def set_dollars_pledged(self):
+        self.project_dict['DollarsPledged'] = self.properties_dict.get('project').\
+            get('project_current_amount_pledged_usd')
+
+    def set_dollars_goal(self):
+        self.project_dict['DollarsGoal'] = self.properties_dict.get('project').get('project_goal_usd')
+
+    def set_num_backers(self):
+        self.project_dict['NumBackers'] = self.data_project.get('backers_count')
     
-    def setNumBackers(self):
-        pass
-    
-    def setDaysToGo(self):
-        pass
+    def set_days_to_go(self):
+        today = datetime.now().date()
+        end_day = datetime.fromtimestamp(int(self.data_project.get('deadline'))).date()
+        self.project_dict['DaysToGo'] = (end_day - today).days - 1
 
-    def setAllOrNothing(self):
-        # soup = self.articleRef
-        # allOrNothing = soup.find_all('span', {'data-test-id': "deadline-exists"})  # finds all or nothing text
-        pass
+    def set_all_or_nothing(self):
+        self.project_dict['AllOrNothing'] = self.properties_dict.get('project').get('project_deadline')
 
-    def setArticleRef(self):
-        pass
+    def set_page_soup_and_properties_dict(self):
+        page_html = requests.get(self.get_url()).text
+        self.page_soup = BeautifulSoup(page_html, 'lxml')
+        properties_script = self.page_soup.find_all('script')[4]
+        self.properties_dict = json.loads(properties_script.text.
+                                          split('window.ksr_track_properties =')[-1].splitlines()[0])
 
-    def jsonCreator(self):
-        pass
+    def get_article_details(self):
+        return self.project_dict
 
-    def getJson(self):
-        return self.json
+    def get_url(self):
+        return self.project_dict['url']
--- creepyCrawlers.py
+++ creepyCrawlers.py
@@ -1,28 +1,34 @@
 from bs4 import BeautifulSoup
-import sys
-import articleObject as cc
+import articleObject as ac
 import requests
 import json
+import time
+
+NUM_PAGES = 25  # 25*12 == 300
+START_URL = 'https://www.kickstarter.com/discover/advanced?category_id=16&woe_id=0&sort=magic&seed=2727457&page='
+SLEEP_TIME_BETWEEN_REQUESTS = 3
 
 
-def main(htmlFile):
-    articlesObj = []
-    html = requests.get(htmlFile).text
+def main():
+    article_objects = []
 
-    soup = BeautifulSoup(html, 'lxml')
-    articles = soup.find_all('div', class_="js-react-proj-card grid-col-12 grid-col-6-sm grid-col-4-lg")
-    for article in articles:
-        articlesObj.append(cc.articleParams(article))
-    jsonList = []
-    for article in articlesObj:
-        jsonList.append(article.getJson())
-    jsonDict = {"records": {"record": jsonList}}
-    # json_object = json.dumps(jsonDict, indent=4)
+    for page in range(1, NUM_PAGES + 1):
+        main_page_url = f'{START_URL}{page}'
+        main_page_html = requests.get(main_page_url).text
+        main_page_soup = BeautifulSoup(main_page_html, 'lxml')
+        articles = main_page_soup.find_all('div', class_="js-react-proj-card grid-col-12 grid-col-6-sm grid-col-4-lg")
+
+        for article in articles:
+            article_object = ac.ArticleObject(article)
+            article_objects.append(article_object)
+            time.sleep(SLEEP_TIME_BETWEEN_REQUESTS)
+
+    json_list = [article.get_article_details() for article in article_objects]
+    json_dict = {"records": {"record": json_list}}
+
     with open("sample.json", "w") as outfile:
-        json.dump(jsonDict, outfile)
-    # print(json_object)
+        json.dump(json_dict, outfile, indent=2)
+
 
 if __name__ == "__main__":
-    # htmlFile = sys.argv[1]
-    htmlFile = 'https://www.kickstarter.com/discover/advanced?category_id=16&woe_id=0&sort=magic&seed=2727457&page=1'
-    main(htmlFile)
+    main()
